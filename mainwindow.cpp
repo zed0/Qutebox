eaea01d1d2a8e45010f2dc8b15abea5c4daba3bd
Ben Falconer <ben@falconers.me.uk>
2010-12-30 22:43:27 +0000

    Added some UI components and a basic user model.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,28 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent):QMainWindow(parent),ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+	ui->setupUi(this);
+
+	userModel = new QStringListModel();
+	ui->userListView->setModel(userModel);
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+	delete ui;
+}
+
+void MainWindow::addUser()
+{
+	QString username = ui->usernameLineEdit->text();
+	if(username == "")
+	{
+		return;
+	}
+	int n = userModel->rowCount();
+	userModel->insertRows(n,1);
+	QModelIndex index = userModel->index(n);
+	userModel->setData(index,username);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,21 +2,27 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QStringListModel>
+#include <QStringList>
 
 namespace Ui {
-    class MainWindow;
+	class MainWindow;
 }
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
+	explicit MainWindow(QWidget *parent = 0);
+	~MainWindow();
+
+public slots:
+	void addUser();
 
 private:
-    Ui::MainWindow *ui;
+	QStringListModel *userModel;
+	Ui::MainWindow *ui;
 };
 
 #endif // MAINWINDOW_H
